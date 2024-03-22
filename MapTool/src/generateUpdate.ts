897cf9c4803c0a59715f72598b4ef5674ce8c1ea
Alina Shumann <ashumann13@gmail.com>
2024-03-22 11:07:10 -0500

    Small updates to site

--- MapTool/src/generateUpdate.ts
+++ MapTool/src/generateUpdate.ts
@@ -1,6 +1,5 @@
 import { glob } from 'glob'
 import { readFile, writeFile } from 'node:fs/promises'
-import { dirname } from 'node:path'
 import YAML from 'yaml'
 
 interface ChangeLogEntry {
@@ -20,37 +19,28 @@ interface Definition {
 }
 
 interface Mod {
+  id: string
   name: string
-  desccription: string
+  desc: string
   version: string
   downloadUrl: string
   changelog: ChangeLogEntry[]
 }
 
 async function run() {
+  const output = {} as Record<string,Mod>
+  const modList = await glob('../*/mod.yaml').then(files => Promise.all(files.map(async f => YAML.parse(await readFile(f, 'utf8')) as Mod)))
+  const mods = Object.fromEntries(modList.map(mod => [mod.id, mod]))
+  for(const mod of Object.values(mods)) {
+    const name = mod.id.split('.')[1]
+    mod.downloadUrl = `https://github.com/AlinaNova21/Railroader-Mods/releases/download/v${mod.version}/${name}_${mod.version}.zip`
+  } 
+  await writeFile('../Site/static/mods.json', JSON.stringify(mods, null, 2))
   const update = {} as Update
-  const mods = {} as Record<string,Mod>
-  const changelogs = await glob('../**/changelog.yaml', { ignore: ['**/node_modules/**'] })
-  for (const changelogFile of changelogs) {
-    const dir = dirname(changelogFile)
-    const changelog = YAML.parse(await readFile(changelogFile, 'utf8')) as ChangeLogEntry[]
-    const definition = JSON.parse(await readFile(`${dir}/definition.json`, 'utf8')) as Definition
-    const version = changelog[0].version
-    const [,name] = definition.id.split('.')
-    update[definition.id] = {
-      version,
-      changelog
-    }
-    mods[definition.id] = {
-      name,
-      desccription: '',
-      changelog,
-      version,
-      downloadUrl: `https://github.com/AlinaNova21/Railroader-Mods/releases/download/v${version}/${name}_${version}.zip`
-    }
+  for(const { id, version, changelog } of modList) {
+    update[id] = { version, changelog }
   }
   await writeFile('../Site/static/update.json', JSON.stringify(update, null, 2))
-  await writeFile('../Site/static/mods.json', JSON.stringify(mods, null, 2))
 }
 
 run().catch(console.error)
