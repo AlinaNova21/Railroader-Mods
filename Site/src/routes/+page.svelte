bdb3d1345f66cad4bb14021e749c0366dc2950be
Alina Shumann <ashumann13@gmail.com>
2024-07-06 18:22:20 -0500

    Site update

--- Site/src/routes/+page.svelte
+++ Site/src/routes/+page.svelte
@@ -1,40 +1,33 @@
 <script lang="ts">
-  import { Button, Card, CardBody, CardGroup, CardHeader, CardText, CardTitle, Container } from '@sveltestrap/sveltestrap'
+  import { Button, Col, Container, Row } from '@sveltestrap/sveltestrap'
   import type { PageData } from './$types'
 	export let data: PageData;
 </script>
 <Container fluid>
   <h1 class="text-center">Alina's Railroader Mods:</h1>
-  <div>All mods are installed by and require <a href="https://railroader.stelltis.ch/">Railloader</a> 1.6+.</div>
-  <div>For AlinasMapMod, <a href="https://railroader.stelltis.ch/mods/strange-customs">StrangeCustoms</a> 1.6+ (Beta) is required.</div>
-  <Container fluid>
-    <CardGroup>
-      {#each data.mods as mod}
-      <Card>
-        <CardHeader>
-          <CardTitle>{ mod.name }</CardTitle>
-        </CardHeader>
-        <CardBody>
-          <CardText>
-            { mod.desc }
-          </CardText>
-
-          <Button href={`/mods/${mod.id}`} color="primary">Details</Button>
+  <div>All mods are installed by and require <a href="https://railroader.stelltis.ch/">Railloader</a> 1.8+.</div>
+  <div>For AlinasMapMod, <a href="https://railroader.stelltis.ch/mods/strange-customs">StrangeCustoms</a> 1.6+ is required.</div>
+  <Container md>
+    <Row>
+      <hr />
+    </Row>
+    {#each data.mods as mod}
+    <Row>
+      <Col>
+        <h2>{ mod.name }</h2>
+      </Col>
+      <Col>{ mod.desc }</Col>
+      <Col xs="auto">
+        <div>Version: v{ mod.version }</div>
+        <div>
+          <Button href={`/mods/${mod.id}/changelog`} color="primary">Changelog</Button>
           <Button href={mod.downloadUrl} color="success">Download</Button>
-        </CardBody>
-      </Card>
-      <!-- <h3>Changelog</h3>
-      <div class="container">
-        {#each mod.changelog as c}
-        <Row class="changelog">
-          <Col md="2">{ c.version }:</Col>
-          <Col md="10" text-left>
-            <Markdown source={ c.desc } />
-          </Col>
-        </Row>
-        {/each}
-      </div> -->
-      {/each}
-    </CardGroup>
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <hr />
+    </Row>
+    {/each}
   </Container>
 </Container>
