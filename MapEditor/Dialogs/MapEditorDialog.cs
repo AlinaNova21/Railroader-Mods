a4e356dd48c2e03e44edf0a1cfb81fec7f6cf8b9
Marek Buchar <czbuchi@gmail.com>
2024-07-15 18:35:25 +0200

    bugfix: support for mods with multiple game-graph.json files

--- MapEditor/Dialogs/MapEditorDialog.cs
+++ MapEditor/Dialogs/MapEditorDialog.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using Railloader;
 using Track;
 using UI.Builder;
 using UI.Common;
@@ -9,42 +9,65 @@ namespace MapEditor.Dialogs
 {
   public sealed class MapEditorDialog : DialogBase
   {
-
     public MapEditorDialog() : base("Map Editor", 400, 350, Window.Position.UpperLeft)
     {
-      _ModMixintoList = EditorContext.ModdingContext.GetMixintos("game-graph").ToList();
-      _Names = _ModMixintoList.Select(o => o.Source.ToString()).ToList();
-      _Names.Insert(0, "Select ...");
+      _Sources = EditorContext.ModdingContext.GetMixintos("game-graph")
+        .GroupBy(o => o.Source.ToString(), o => o.Mixinto)
+        .ToDictionary(o => o.Key, o => o.ToList());
+
+      _Mods = _Sources.Keys.ToList();
+      _Mods.Insert(0, "Select ...");
     }
 
-    private readonly List<ModMixinto> _ModMixintoList;
-    private readonly List<string> _Names;
-    private int _SelectedModMixintoIndex;
+    private readonly Dictionary<string, List<string>> _Sources;
+    private readonly List<string> _Mods;
+    private int _SelectedMod;
+    private int _SelectedPatch;
 
     protected override void BuildWindow(UIPanelBuilder builder)
     {
-      builder.AddField("Patch", builder.AddDropdown(_Names, _SelectedModMixintoIndex, o =>
+      builder.AddField("Mod", builder.AddDropdown(_Mods, _SelectedMod, o =>
       {
-        _SelectedModMixintoIndex = o;
+        _SelectedMod = o;
+        _SelectedPatch = 0;
+        EditorContext.CloseMixinto();
         builder.Rebuild();
       })!);
 
-      if (_SelectedModMixintoIndex != 0)
+      if (_SelectedMod != 0)
       {
-        EditorContext.OpenMixinto(_ModMixintoList[_SelectedModMixintoIndex - 1].Mixinto);
+        var mod = _Mods[_SelectedMod];
+        var graphs = _Sources[mod]!.Select(Path.GetFileNameWithoutExtension).ToList();
+        graphs.Insert(0, "Select ...");
 
-        builder.AddField("Prefix", builder.AddInputField(EditorContext.Prefix, value => EditorContext.Prefix = value, "Prefix")!);
-        builder.HStack(stack =>
+        // show second dropdown only if there is more than one graph ...
+        if (graphs.Count > 2)
         {
-          stack.AddButtonCompact("Undo", EditorContext.ChangeManager.Undo);
-          stack.AddButtonCompact("Redo", EditorContext.ChangeManager.Redo);
-          stack.AddButtonCompact("Save", EditorContext.Save);
-          
-        });
-      }
-      else if (EditorContext.PatchEditor != null)
-      {
-        EditorContext.CloseMixinto();
+          builder.AddField("Graph", builder.AddDropdown(graphs, _SelectedPatch, o =>
+          {
+            _SelectedPatch = o;
+            builder.Rebuild();
+          })!);
+        }
+        else
+        {
+          _SelectedPatch = 1;
+        }
+
+        if (_SelectedPatch != 0)
+        {
+          EditorContext.OpenMixinto(_Sources[mod]![_SelectedPatch - 1]!); // -1 because of 'Select ...'
+
+          // IDEA: use $"{mod}_{graphs[_SelectedPatch]}" as prefix? (so player can tell which mod modified what)
+          builder.AddField("Prefix", builder.AddInputField(EditorContext.Prefix, value => EditorContext.Prefix = value, "Prefix")!);
+          builder.AddField("Recorded commands", EditorContext.ChangeManager.Count.ToString, UIPanelBuilder.Frequency.Periodic);
+          builder.HStack(stack =>
+          {
+            stack.AddButtonCompact("Undo", EditorContext.ChangeManager.Undo);
+            stack.AddButtonCompact("Redo", EditorContext.ChangeManager.Redo);
+            stack.AddButtonCompact("Save", EditorContext.Save);
+          });
+        }
       }
 
       builder.AddButton("Rebuild Track", () =>
@@ -59,7 +82,7 @@ namespace MapEditor.Dialogs
 
     public void BuildSettings(UIPanelBuilder builder)
     {
-      builder.AddButton("Keyboard bindings", () => EditorContext.KeyboardSettingsDialog.ShowWindow());
+      builder.AddButton("Keyboard bindings", EditorContext.KeyboardSettingsDialog.ShowWindow);
     }
 
     protected override void AfterWindowClosed()
